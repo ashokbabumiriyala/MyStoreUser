51baf9a3a742159e3d80ece905b75c16866d516a
Venkat Garapani <venkat.garapani@pareteum.com>
2021-07-25 13:29:49 +0530

    updated release version to v1.1.2

--- UserApp/src/environments/environment.prod.ts
+++ UserApp/src/environments/environment.prod.ts
@@ -9,19 +9,19 @@ export const environment = {
   //   'https://my3apitest.itprototypes.com/api/UserAppOperation/',
   // pushTokenServiceUrl: 'https://my3apitest.itprototypes.com/api/PushToken/',
 
-  // authenticationServiceUrl:
-  //   'https://my3api.itprototypes.com/api/UserAuthentication/',
-  // storeMasterServiceUrl: 'https://my3api.itprototypes.com/api/Store/',
-  // serviceMasterServiceUrl: 'https://my3api.itprototypes.com/api/Service/',
-  // adminServiceUrl: 'https://my3api.itprototypes.com/api/Admin/',
-  // userOperationServiceUrl:
-  //   'https://my3api.itprototypes.com/api/UserAppOperation/',
-  // pushTokenServiceUrl: 'https://my3api.itprototypes.com/api/PushToken/',
-  authenticationServiceUrl: 'https://localhost:5001/api/UserAuthentication/',
-  storeMasterServiceUrl: 'https://localhost:5001/api/Store/',
-  userOperationServiceUrl: 'https://localhost:5001/api/UserAppOperation/',
-  serviceMasterServiceUrl: 'https://localhost:5001/api/Service/',
-  adminServiceUrl: 'https://localhost:5001/api/Admin/',
-  pushTokenServiceUrl: 'https://localhost:5001/api/PushToken/',
+  authenticationServiceUrl:
+    'https://my3api.itprototypes.com/api/UserAuthentication/',
+  storeMasterServiceUrl: 'https://my3api.itprototypes.com/api/Store/',
+  serviceMasterServiceUrl: 'https://my3api.itprototypes.com/api/Service/',
+  adminServiceUrl: 'https://my3api.itprototypes.com/api/Admin/',
+  userOperationServiceUrl:
+    'https://my3api.itprototypes.com/api/UserAppOperation/',
+  pushTokenServiceUrl: 'https://my3api.itprototypes.com/api/PushToken/',
+  // authenticationServiceUrl: 'https://localhost:5001/api/UserAuthentication/',
+  // storeMasterServiceUrl: 'https://localhost:5001/api/Store/',
+  // userOperationServiceUrl: 'https://localhost:5001/api/UserAppOperation/',
+  // serviceMasterServiceUrl: 'https://localhost:5001/api/Service/',
+  // adminServiceUrl: 'https://localhost:5001/api/Admin/',
+  // pushTokenServiceUrl: 'https://localhost:5001/api/PushToken/',
   razorPaymentkey: 'rzp_live_BCrYXIrnh8hw2v',
 };
--- UserApp/src/environments/environment.ts
+++ UserApp/src/environments/environment.ts
@@ -12,21 +12,23 @@ export const environment = {
   // adminServiceUrl: 'https://localhost:44337/api/Admin/',
 
   // test
-  authenticationServiceUrl:
-    'https://my3apitest.itprototypes.com/api/UserAuthentication/',
-  storeMasterServiceUrl: 'https://my3apitest.itprototypes.com/api/Store/',
-  serviceMasterServiceUrl: 'https://my3apitest.itprototypes.com/api/Service/',
-  adminServiceUrl: 'https://my3apitest.itprototypes.com/api/Admin/',
-  userOperationServiceUrl:
-    'https://my3apitest.itprototypes.com/api/UserAppOperation/',
-  pushTokenServiceUrl: 'https://my3apitest.itprototypes.com/api/PushToken/',
+  // authenticationServiceUrl:
+  //   'https://my3apitest.itprototypes.com/api/UserAuthentication/',
+  // storeMasterServiceUrl: 'https://my3apitest.itprototypes.com/api/Store/',
+  // serviceMasterServiceUrl: 'https://my3apitest.itprototypes.com/api/Service/',
+  // adminServiceUrl: 'https://my3apitest.itprototypes.com/api/Admin/',
+  // userOperationServiceUrl:
+  //   'https://my3apitest.itprototypes.com/api/UserAppOperation/',
+  // pushTokenServiceUrl: 'https://my3apitest.itprototypes.com/api/PushToken/',
 
-  // authenticationServiceUrl: 'https://my3api.itprototypes.com/api/UserAuthentication/',
-  // storeMasterServiceUrl: 'https://my3api.itprototypes.com/api/Store/',
-  // serviceMasterServiceUrl: 'https://my3api.itprototypes.com/api/Service/',
-  // adminServiceUrl: 'https://my3api.itprototypes.com/api/Admin/',
-  // userOperationServiceUrl: 'https://my3api.itprototypes.com/api/UserAppOperation/',
-  // pushTokenServiceUrl: 'https://my3api.itprototypes.com/api/PushToken/',
+  authenticationServiceUrl:
+    'https://my3api.itprototypes.com/api/UserAuthentication/',
+  storeMasterServiceUrl: 'https://my3api.itprototypes.com/api/Store/',
+  serviceMasterServiceUrl: 'https://my3api.itprototypes.com/api/Service/',
+  adminServiceUrl: 'https://my3api.itprototypes.com/api/Admin/',
+  userOperationServiceUrl:
+    'https://my3api.itprototypes.com/api/UserAppOperation/',
+  pushTokenServiceUrl: 'https://my3api.itprototypes.com/api/PushToken/',
 
   // authenticationServiceUrl: 'https://localhost:5001/api/UserAuthentication/',
   // storeMasterServiceUrl: 'https://localhost:5001/api/Store/',
